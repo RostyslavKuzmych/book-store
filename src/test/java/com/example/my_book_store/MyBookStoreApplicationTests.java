209ed5e44e7af3edca181df1b65aa56ab16af726
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-11-11 21:21:54 +0200

    created the infrastructure

---
+++ src/test/java/com/example/my_book_store/MyBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_book_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
